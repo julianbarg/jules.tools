89c591b43a4e063f2011c568935de79e6a77fead
Julian Barg <barg.julian@gmail.com>
2023-11-06 13:48:57 -0500

    Fixed more issues.

--- R/chatgpt_xl.R
+++ R/chatgpt_xl.R
@@ -1,10 +1,10 @@
 #' Access Extended Array of OpenAI Models via OpenRouter
 #'
 #' @param api_key A character string. Your OpenAI API key.
+#' @param referer A character string that is send as the referer. Set to your personal website or e.g. http://localhost:3000 for testing (see https://openrouter.ai/docs).
 #' @param role A character string specifying the role for the interaction.
 #' @param content A character string or list specifying the content for the interaction.
 #' @param model A character string specifying the model to be used. Default is "gpt-3.5-turbo".
-#' @param api_endpoint A character string. The API endpoint for OpenAI. Default is OpenAI's endpoint.
 #'
 #' @return A list containing the model's response.
 #'
@@ -16,18 +16,19 @@
 #' @export
 chatgpt_xl <- function(
   api_key,
+  referer,
   role,
   content,
-  model = "gpt-3.5-turbo",
-  api_endpoint = "https://api.openai.com/v1/chat/completions") {
+  model = "gpt-3.5-turbo"
+  ) {
 
   openrouter_url <- "https://openrouter.ai/api/v1/chat/completions"
 
   headers <- c(
     "Content-Type" = "application/json",
     "Authorization" = paste("Bearer", api_key),
-    "HTTP-Referer" = api_endpoint,
-    "X-Title" = "OpenAI"
+    # "X-Title" = "optional",
+    "HTTP-Referer" = referer
   )
 
   data <- list(
@@ -41,7 +42,8 @@ chatgpt_xl <- function(
     url = openrouter_url,
     httr::add_headers(.headers = headers),
     body = jsonlite::toJSON(data, auto_unbox = TRUE),
-    encode = "json"
+    encode = "json",
+    httr::timeout(1500)
   )
 
   return(response)
